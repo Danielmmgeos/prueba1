f7495c2cab975501cbbe279c2261f33df2a81e7f
Danielmmgeos <62635044+Danielmmgeos@users.noreply.github.com>
2024-07-24 17:48:35 -0600

    Update modal.js

--- modal.js
+++ modal.js
@@ -1,23 +1,23 @@
-
+// Obtener el modal
 var modal = document.getElementById("modalAcercaDe");
 
-
+// Obtener el botón que abre el modal
 var btn = document.querySelector('nav ul li a[data-toggle="modal"]');
 
-
+// Obtener el elemento <span> que cierra el modal
 var span = document.getElementsByClassName("close")[0];
 
-
+// Cuando el usuario hace clic en el botón, abre el modal
 btn.onclick = function() {
     modal.style.display = "block";
 }
 
-
+// Cuando el usuario hace clic en <span> (x), cierra el modal
 span.onclick = function() {
     modal.style.display = "none";
 }
 
-
+// Cuando el usuario hace clic en cualquier lugar fuera del modal, lo cierra
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
