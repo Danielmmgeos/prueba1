61c664a08512e16318764864b424bd78ee7b0d05
Danielmmgeos <62635044+Danielmmgeos@users.noreply.github.com>
2024-07-24 17:38:30 -0600

    Create modal.js

---
+++ modal.js
@@ -0,0 +1,25 @@
+
+var modal = document.getElementById("modalAcercaDe");
+
+
+var btn = document.querySelector('nav ul li a[data-toggle="modal"]');
+
+
+var span = document.getElementsByClassName("close")[0];
+
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
