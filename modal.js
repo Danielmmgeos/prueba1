07b21c65086e325dfe19d6609d77e9ad2422de14
Danielmmgeos <62635044+Danielmmgeos@users.noreply.github.com>
2024-07-25 22:33:50 -0600

    Update modal.js

--- modal.js
+++ modal.js
@@ -1,22 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var modal = document.getElementById("modalAcercaDe");
+    var btn = document.querySelector('nav ul li a[data-toggle="modal"]');
+    var span = document.getElementsByClassName("close")[0];
 
-var modal = document.getElementById("modalAcercaDe");
-
-
-var btn = document.querySelector('nav ul li a[data-toggle="modal"]');
-
-
-var span = document.getElementsByClassName("close")[0];
-
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (btn) {
+        btn.onclick = function() {
+            modal.style.display = "block";
+        }
     }
-}
+
+    if (span) {
+        span.onclick = function() {
+            modal.style.display = "none";
+        }
+    }
+
+    window.onclick = function(event) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    }
+});
