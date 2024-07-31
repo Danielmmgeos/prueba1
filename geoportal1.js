0542af91fdefa935a5893f984514a155951b7cdc
CFernandoSM <132521878+CFernandoSM@users.noreply.github.com>
2024-07-30 21:57:04 -0600

    Cambios a modal
    Se hace funcional el modal

--- geoportal1.js
+++ geoportal1.js
@@ -241,3 +241,29 @@ document.addEventListener('DOMContentLoaded', function() {
     });
     });    
 
+
+// CONTROL PARA MODAL
+
+document.addEventListener("DOMContentLoaded", function() {
+    var modal = document.getElementById("modalAcercaDe");
+    var btn = document.querySelector('nav ul li a[data-toggle="modal"]');
+    var span = document.getElementsByClassName("close")[0];
+
+    if (btn) {
+        btn.onclick = function() {
+            modal.style.display = "block";
+        }
+    }
+
+    if (span) {
+        span.onclick = function() {
+            modal.style.display = "none";
+        }
+    }
+
+    window.onclick = function(event) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    }
+});
